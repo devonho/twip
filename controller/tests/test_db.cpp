3cba2a58e9e2f7c718f012affd99df26fc8aba57
devonho <devonho@msn.com>
2023-02-05 22:37:09 +0800

    Refactor for Catch2 v 2

--- controller/tests/test_db.cpp
+++ controller/tests/test_db.cpp
@@ -1,4 +1,6 @@
-#include <catch2/catch_test_macros.hpp>
+#define CATCH_CONFIG_MAIN
+
+#include  <catch2/catch.hpp>
 
 TEST_CASE( "1: All test cases reside in other .cpp files (empty)", "[multi-file:1]" ) {
 }
